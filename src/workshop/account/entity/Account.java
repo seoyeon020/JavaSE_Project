857b076ce243582dbf254463f38333579d01f114
seoyeon020 <ksheee7000@naver.com>
2026-03-03 13:28:12 +0900

    account update

---
+++ src/workshop/account/entity/Account.java
@@ -0,0 +1,46 @@
+package workshop.account.entity;
+
+public class Account {
+	private String custId;
+	private String acctId;
+	private int balance;
+	
+	//setter method (멤버변수 값 변경)
+//	public void setBalance(int balance) {
+//		balance = balance;
+//	}
+	
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+	
+	public void SetCustId(String custId) {
+		this.custId = custId;
+	}
+	
+	//getter method (멤버변수 값 조회)
+	public String getCustId() {
+		return custId;
+	}
+	
+	public String getAcctId() {
+		return acctId;
+	}
+	
+	public int getBalance() {
+		return  balance;
+	}
+	
+	//입금
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+	//출금
+	public void withdraw(int amount) {
+		if(amount > balance) {
+			System.out.println("잔액 부족");
+		}
+		this.balance -= amount;
+	}
+
+}
