75f5aa0439d8df7376b8263d67e973fc4f5ed10b
seoyeon020 <ksheee7000@naver.com>
2026-03-09 17:37:14 +0900

    [실습3] bank update

--- src/mylab/bank/control/BankDemo.java
+++ src/mylab/bank/control/BankDemo.java
@@ -1,5 +1,67 @@
 package mylab.bank.control;
 
+import mylab.bank.entity.*;
+import mylab.bank.exception.*;
+
 public class BankDemo {
+	public static void main(String[] args) {
+		Bank bank = new Bank();
+		
+		try {
+			System.out.println("==== 계좌 생성 ====");
+			String ac1 = bank.createSavingsAccount("홍길동", 10000, 3);
+			System.out.println("저축 계좌가 생성되었습니다: " + bank.findAccount(ac1));
+			String ac2 = bank.createCheckingAccount("김철수", 20000, 5000);
+			System.out.println("체킹 계좌가 생성되었습니다: " + bank.findAccount(ac2));
+			String ac3 = bank.createSavingsAccount("이영희", 30000, 2);
+			System.out.println("저축 계좌가 생성되었습니다: " + bank.findAccount(ac3));
+			
+			System.out.println("\n==== 모든 계좌 목록 ====");
+			bank.printAllAccounts();
+			System.out.println("===================");
+			
+			System.out.println("\n==== 입금/출금 테스트 ===="); //홍길동 계좌
+			Account hong = bank.findAccount(ac1);
+			hong.deposit(5000);
+			System.out.println("5000.0원이 입금되었습니다. 이름: " + hong.getOwnerName() + "  현재 잔액: " + hong.getBalance() + "원 ");
+			
+			Account kim = bank.findAccount(ac2); //김철수 계좌 
+			kim.withdraw(3000);
+			System.out.println("3000.0원이 출금되었습니다. 이름: " + kim.getOwnerName() + "  현재 잔액: " + kim.getBalance() + "원");
+			
+			System.out.println("\n==== 이자 적용 테스트 ====");
+			((SavingsAccount) hong).applyInterest();
+			
+			System.out.println("\n==== 계좌 이체 테스트 ====");
+			Account lee = bank.findAccount(ac3);
+			bank.transfer(ac3, ac2, 5000); //
+			
+			System.out.println("5000.0원이 출금되었습니다. 이름: " + lee.getOwnerName() + "  현재 잔액: " + lee.getBalance() + "원");
+			System.out.println("5000.0원이 입금되었습니다. 이름: " + kim.getOwnerName() + "  현재 잔액: " + kim.getBalance() + "원");
+			
+			System.out.println("\n==== 모든 계좌 목록 ====");
+			bank.printAllAccounts();
+			System.out.println("===================");
+			
+			//예외 발생: 김철수 계좌 5000원 한도 
+			bank.findAccount(ac2).withdraw(6000);
+			
+		} catch (WithdrawalLimitExceededException e) {
+			System.out.println("예외 발생: " + e.getMessage());
+		} catch (InsufficientBalanceException e) {
+			System.out.println("예외 발생: " + e.getMessage());
+		} catch (AccountNotFoundException e) {
+			System.out.println("예외 발생: " + e.getMessage());
+		}
+		
+		//예외 발생: 존재하지 않는 계좌
+		try {
+			bank.findAccount("AC9999");
+		} catch (AccountNotFoundException e) {
+			System.out.println("예외 발생: " + e.getMessage());
+		}
+		
+		
+	}
 
 }
--- src/mylab/bank/entity/Bank.java
+++ src/mylab/bank/entity/Bank.java
@@ -2,20 +2,56 @@ package mylab.bank.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+import mylab.bank.exception.*;
 
 public class Bank {
 	//은행 시스템, 계좌 생성과 관리 담당 
 	private List<Account> accounts; //은행에서 관리하는 계좌 목록 
 	private int nextAccountNumber = 1000; //새계좌 생성시 부여할 다음 계좌번호 
 	
-	public Bank() {
+	public Bank() { //계좌를 저장할 리스트 객체 생성 
 		this.accounts = new ArrayList<>();
 	}
 	
+	//저축 계좌 생성 메서드 
 	public String createSavingsAccount(String name, double balance, double interestRate) {
-		String accNum = "AC" + nextAccountNumber++;
+		String accNum = "AC" + nextAccountNumber++; //계좌번호 생성 
 		accounts.add(new SavingsAccount(accNum, name, balance, interestRate));
 		return accNum;
 	}
-
+	
+	//체킹 계좌 생성 메서드 
+	public String createCheckingAccount(String name, double balance, double limit) {
+		String accNum = "AC" + nextAccountNumber++;
+		accounts.add(new CheckingAccount(accNum, name, balance, limit));
+		return accNum;
+	}
+	
+	//계좌 조회 메서드 
+	public Account findAccount(String accNum) throws AccountNotFoundException {
+		for (Account acc : accounts) {
+			//입력받은 accNum과 실제(getAccountNumber)가 같은지 확인 
+			if (acc.getAccountNumber().equals(accNum)) return acc;
+		}
+		throw new AccountNotFoundException("계좌번호 " + accNum+ "에 해당하는 계좌를 찾을 수 없습니다.");
+	}
+	
+	//계좌 이체 메서드 
+	//fromNum: 송신 계좌번호, toNum: 수신 계좌번호 
+	public void transfer(String fromNum, String toNum, double amount) throws AccountNotFoundException, InsufficientBalanceException {
+		Account from = findAccount(fromNum);
+		Account to = findAccount(toNum);
+		
+		from.withdraw(amount); //출금
+		to.deposit(amount); //입금
+		
+		System.out.println(amount + "원이 " + fromNum + "에서 " + toNum + "로 송금되었습니다.");
+	}
+	
+	//모든 계좌 목록 
+	public void printAllAccounts() {
+		for (Account acc : accounts) {
+			System.out.println(acc.toString());
+		}
+	}
 }
